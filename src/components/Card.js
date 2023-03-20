93082d8167a511905a09ef9c7f593802c7fe62a3
Ron Barak <ronbarak2000@gmail.com>
2023-03-20 17:12:19 +0200

    cards top

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,32 @@
 import VIcon from "assets/images/v-icon.png";
 import DollarSpin from "assets/images/dollar-spin.png";
 
-const Card = ({ img }) => {
+const Card = (props) => {
   return (
     <div className="card">
       <div className="discount">
-        <span className="title">60%</span>
+        <span className="title">{props.discount}</span>
         <span className="subtitle">Discount</span>
       </div>
-      <img className="card-image" src={img} alt="" />
+      <img className="card-image" src={props.img} alt="" />
+      <div className={`best-value ${props.bestValue ? "visible" : ""}`}>
+        Best Value
+      </div>
       <div className="titles">
-        <span className="title">Extended Protection</span>
-        <span className="subtitle">Windows PC Antivirus</span>
+        <span className="title">{props.titles.title}</span>
+        <span className="subtitle">{props.titles.subtitle}</span>
       </div>
       <div className="price">
-        <span className="title">Only $1.67/month</span>
-        <div className="subtitle">
-          Billed <span className="new-price"> $19.99 </span>{" "}
-          <span className="old-price"> $49.99 </span> for the first 1 year
-        </div>
+        <span className="title">{props.price.title}</span>
+        <div
+          className="subtitle"
+          dangerouslySetInnerHTML={{ __html: props.price.subtitle }}
+        />
       </div>
       <div className="buy-now">
-        <button className="btn">Buy Now</button>
+        <button className={`btn ${props.buyNowGlow ? "glow" : ""}`}>
+          Buy Now
+        </button>
         <span className="subtitle">
           <img src={DollarSpin} alt="" />
           30-day money back guarantee
@@ -29,47 +34,24 @@ const Card = ({ img }) => {
       </div>
       <div className="seperator" />
       <div className="includes">
-        <span className="title">Essential Protection includes:</span>
+        <span className="title">{props.includes.title}</span>
         <div className="list">
-          <div className="list-item">
-            <img className="list-item-image" src={VIcon} height={13} alt="" />
-            <div className="list-item-text">
-              <span className="text-bold">Powerful malware engine:</span>
-              <span className="text">
-                Adaptive, real-time malware engine monitors andeliminates
-                threats before they reach your PC
-              </span>
+          {props.includes.list.map((item) => (
+            <div className={`list-item ${item.glow ? "glow" : ""}`}>
+              <img className="list-item-image" src={VIcon} height={13} alt="" />
+              <div className="list-item-text">
+                <span
+                  className="text-bold"
+                  style={{
+                    color: item.textBoldColor ? item.textBoldColor : "initial",
+                  }}
+                >
+                  {item.textBold}
+                </span>
+                {item.text && <span className="text">{item.text}</span>}
+              </div>
             </div>
-          </div>
-          <div className="list-item">
-            <img className="list-item-image" src={VIcon} height={13} alt="" />
-            <div className="list-item-text">
-              <span className="text-bold">Easy to use, easy on your PC:</span>
-              <span className="text">
-                Intuitive and simple UI with light, customizable scans
-              </span>
-            </div>
-          </div>
-          <div className="list-item">
-            <img className="list-item-image" src={VIcon} height={13} alt="" />
-            <div className="list-item-text">
-              <span className="text-bold">100% malware immunity:</span>
-              <span className="text">
-                Independent lab tests show that Intego eradicates 100% of
-                malware
-              </span>
-            </div>
-          </div>
-          <div className="list-item">
-            <img className="list-item-image" src={VIcon} height={13} alt="" />
-            <div className="list-item-text">
-              <span className="text-bold">Ransomware protection:</span>
-              <span className="text">
-                Protects your personal data from being hacked and encrypted for
-                a ransom
-              </span>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
       <div className="footer-card">
--- src/components/CardsWrapper.js
+++ src/components/CardsWrapper.js
@@ -2,13 +2,120 @@ import Card from "./Card";
 import Essential from "assets/images/essential.png";
 import Advanced from "assets/images/advanced.png";
 import Extended from "assets/images/advanced.png";
+import { useMemo } from "react";
 
 const CardsWrapper = () => {
+  const data = useMemo(
+    () => [
+      {
+        img: Essential,
+        discount: "60%",
+        titles: {
+          title: "Essential Protection",
+          subtitle: "Windows PC Antivirus",
+        },
+        price: {
+          title: "Only $1.67/month",
+          subtitle: `Billed <span class="new-price">$19.99</span> <span class="old-price">$49.99</span> for the first 1 year`,
+        },
+        includes: {
+          title: "Essential Protection includes:",
+          list: [
+            {
+              textBold: "Powerful malware engine:",
+              text: "Adaptive, real-time malware engine monitors andeliminates threats before they reach your PC",
+            },
+            {
+              textBold: "Easy to use, easy on your PC:",
+              text: "Intuitive and simple UI with light, customizable scans",
+            },
+            {
+              textBold: "100% malware immunity:",
+              text: "Independent lab tests show that Intego eradicates 100% of malware",
+            },
+            {
+              textBold: "Ransomware protection:",
+              text: "Protects your personal data from being hacked and encrypted for a ransom",
+            },
+          ],
+        },
+      },
+      {
+        img: Advanced,
+        discount: "65%",
+        titles: {
+          title: "Advanced Protection",
+          subtitle: "Online Privacy Features:",
+        },
+        price: {
+          title: "Only $2.50/month",
+          subtitle: `Billed <span class="new-price">$29.99</span> <span class="old-price">$84.99</span> for the first 1 year`,
+        },
+        includes: {
+          title: "Advanced Protection includes:",
+          list: [
+            {
+              textBold: "Essential  Protection Plan",
+              glow: true,
+            },
+            {
+              textBold: "Secure Wi-Fi protection:",
+              text: "Secure your connection when connected to public Wi-Fi or low security networks",
+            },
+            {
+              textBold: "Protect your Privacy:",
+              text: "Keep your personal data safe & private",
+            },
+            {
+              textBold: "Browse without boundaries:",
+              text: "Intego VPN opens up the web for streaming & browsing",
+            },
+          ],
+        },
+      },
+      {
+        img: Extended,
+        discount: "70%",
+        buyNowGlow: true,
+        bestValue: true,
+        titles: {
+          title: "Extended Protection",
+          subtitle: "Best Offer you will get!",
+        },
+        price: {
+          title: "Only $2.08/month",
+          subtitle: `Billed <span class="new-price">$49.99</span> <span class="old-price">$169.99</span> for the first 2 year`,
+        },
+        includes: {
+          title: "Extended Protection includes:",
+          list: [
+            {
+              textBold: "Get 70% OFF!",
+              textBoldColor: "#F72928",
+            },
+            {
+              textBold: "2 years protection",
+            },
+            {
+              textBold: "Essential  Protection Plan",
+              glow: true,
+            },
+            {
+              textBold: "Advanced  Protection Plan",
+              glow: true,
+            },
+          ],
+        },
+      },
+    ],
+    []
+  );
+
   return (
     <div className="cards-wrapper">
-      <Card img={Essential} />
-      <Card img={Advanced} />
-      <Card img={Extended} />
+      {data.map((item) => (
+        <Card {...item} />
+      ))}
     </div>
   );
 };
